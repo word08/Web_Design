c4855a59931871ac8b78b3c164bc31c7014a9931
word08 <99589114+word08@users.noreply.github.com>
2022-05-07 20:32:19 +0800

    php

---
+++ week_20220424/p20220430_6.php
@@ -0,0 +1,10 @@
+<?php 
+    $foo="5bar";
+    $bar= true;
+   echo  gettype($foo);
+    echo"<br>";
+    echo settype($bar, "string");
+    echo"<br>";
+    echo var_dump($bar);
+    
+?>
---
+++ week_20220424/p20220430_8.php
@@ -0,0 +1,2 @@
+Note that a variable may be evaluated with different values in certain situations, depending on what type it is at the time. For more information, see the section on Type Juggling.
+The type comparison tables may also be useful, as they show examples of various type-related comparisons.
