1af2560b7ab88e6af8d3151f0fe2a2a1b0e5495c
word08 <99589114+word08@users.noreply.github.com>
2022-05-07 20:34:41 +0800

    Add files via upload

--- week_20220424/p20220430_8.php
+++ week_20220424/p20220430_8.php
@@ -1,2 +1,3 @@
 Note that a variable may be evaluated with different values in certain situations, depending on what type it is at the time. For more information, see the section on Type Juggling.
 The type comparison tables may also be useful, as they show examples of various type-related comparisons.
+請注意，在某些情況下，可能會使用不同的值對變量進行評估，具體取決於當時的類型。有關更多信息，請參閱類型雜耍部分。類型比較表也可能很有用，因為它們顯示了各種類型相關比較的示例。我去他網頁翻譯下來的
